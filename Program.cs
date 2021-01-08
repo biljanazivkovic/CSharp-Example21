75894f0988ed94b12cbceb1ab6acb54331ea98f8
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:41:30 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ex21
+{
+    /*
+    enum Level 
+    {
+        Low,
+        Medium,
+        High
+    }
+    */
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+        static void Main(string[] args)
+        {
+            //int myVar = (int) Level.Medium;
+            Level myVar = Level.Medium;
+            switch (myVar)
+            {
+                case Level.Low:
+                    Console.WriteLine("Low level");
+                    break;
+                case Level.Medium:
+                    Console.WriteLine("Medium level");
+                    break;
+                case Level.High:
+                    Console.WriteLine("High level");
+                    break;
+            }
+        }
+    }
+}
